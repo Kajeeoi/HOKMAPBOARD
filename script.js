fa7f2c65c26f116d314230d9c219b0ee41e2c2db
Kajeeoi <145242200+Kajeeoi@users.noreply.github.com>
2026-07-18 12:14:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,166 @@ fullscreenBtn.onclick = ()=>{
     }
 
 };
+// ==========================================
+// DRAG SYSTEM
+// ==========================================
+
+let activeRole = null;
+
+let offsetX = 0;
+let offsetY = 0;
+
+roles.forEach(role=>{
+
+    role.addEventListener("pointerdown",dragStart);
+
+});
+
+
+function dragStart(e){
+
+    activeRole = e.target;
+
+    activeRole.setPointerCapture(e.pointerId);
+
+    const rect = activeRole.getBoundingClientRect();
+
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+
+    activeRole.style.zIndex = 9999;
+
+}
+
+
+document.addEventListener("pointermove",dragMove);
+
+function dragMove(e){
+
+    if(!activeRole) return;
+
+    const boardRect = board.getBoundingClientRect();
+
+    let x = e.clientX-boardRect.left-offsetX;
+    let y = e.clientY-boardRect.top-offsetY;
+
+    x = Math.max(0,Math.min(x,board.clientWidth-activeRole.offsetWidth));
+
+    y = Math.max(0,Math.min(y,board.clientHeight-activeRole.offsetHeight));
+
+    activeRole.style.position="absolute";
+
+    activeRole.style.left=x+"px";
+
+    activeRole.style.top=y+"px";
+
+}
+
+
+document.addEventListener("pointerup",()=>{
+
+    if(!activeRole) return;
+
+    activeRole.style.zIndex=10;
+
+    activeRole=null;
+
+});
+
+// ==========================================
+// RESET
+// ==========================================
+
+function resetIcons(){
+
+    const blue = [
+
+        "blue-jgl",
+        "blue-mid",
+        "blue-farm",
+        "blue-clash",
+        "blue-roam"
+
+    ];
+
+    const red = [
+
+        "red-jgl",
+        "red-mid",
+        "red-farm",
+        "red-clash",
+        "red-roam"
+
+    ];
+
+    blue.forEach(id=>{
+
+        const icon=document.getElementById(id);
+
+        icon.removeAttribute("style");
+
+    });
+
+    red.forEach(id=>{
+
+        const icon=document.getElementById(id);
+
+        icon.removeAttribute("style");
+
+    });
+
+}
+
+resetBtn.onclick = resetIcons;
+
+window.onload = resetIcons;
+
+// ==========================================
+// SHORTCUT
+// ==========================================
+
+document.addEventListener("keydown",(e)=>{
+
+    switch(e.key.toLowerCase()){
+
+        case "f":
+
+            fullscreenBtn.click();
+
+            break;
+
+        case "r":
+
+            resetBtn.click();
+
+            break;
+
+        case "1":
+
+            desktopBtn.click();
+
+            break;
+
+        case "2":
+
+            tabletBtn.click();
+
+            break;
+
+        case "3":
+
+            mobileBtn.click();
+
+            break;
+
+    }
+
+});
+
+
+// Disable Browser Drag
+document.querySelectorAll("img").forEach(img=>{
+
+    img.draggable=false;
+
+});
